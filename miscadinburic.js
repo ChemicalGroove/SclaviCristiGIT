3cb724a7c87f7772bba6d6820386e7db817daafc
ZanZan <rusanrazvan@gmail.com>
2016-07-13 15:25:23 +0300

    schimbat
    mata

---
+++ miscadinburic.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+function Start () {
+
+	var transform = GetComponent.<Transform>();
+
+}
+
+function Update () {
+
+	if ( Input.GetKey ("a") ) {
+
+		transform.position.x += 0.1;
+
+	}
+
+	if ( Input.GetKey ("d") ) {
+
+		transform.position.x -= 0.1;
+
+	}
+
+	if ( Input.GetKey ("w") ) {
+
+		transform.position.y += 0.1;
+
+	}
+
+
+}
