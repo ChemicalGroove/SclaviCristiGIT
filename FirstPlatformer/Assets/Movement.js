39cf4bc64f4b0d24f81a53cae3576d3378f1c19a
KrostyBala <mrslayedpc@gmail.com>
2016-07-16 16:29:43 +0300

    FirstPlatformerGame
    Asta este jocul cu background si animatiile la caracter :)

---
+++ FirstPlatformer/Assets/Movement.js
@@ -0,0 +1,37 @@
+﻿#pragma strict
+
+	
+
+function Start () {
+
+	var transform = GetComponent.<Transform>();
+
+}
+
+function Update () {
+
+	
+	if ( Input.GetKey ("d") ) {
+
+
+		GetComponent.<Animation>().CrossFade("WalkSclav");
+		transform.position.x += 0.03;
+
+	}
+
+	if ( Input.GetKey ("a") ) {
+
+
+		transform.position.x -= 0.03;
+
+	}
+
+	if ( Input.GetKey ("w") ) {
+
+
+		transform.position.y += 0.03;
+
+	}
+
+
+}
